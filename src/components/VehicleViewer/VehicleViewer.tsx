f037308a6df6745393eac63dfb40ff5edacfa089
TJ07\charl <charles.deukett@hotmail.com>
2026-04-28 11:32:53 +1200

    fix lighting

--- src/components/VehicleViewer/VehicleViewer.tsx
+++ src/components/VehicleViewer/VehicleViewer.tsx
@@ -1,18 +1,9 @@
-import { Canvas, useThree } from "@react-three/fiber";
+import { Canvas } from "@react-three/fiber";
 import { OrbitControls, Html, Environment } from "@react-three/drei";
-import { useEffect } from "react";
 import { Suspense } from "react";
 import Tesla from "./Tesla";
 
 import "./VehicleViewer.scss";
-
-function SceneEnvironment({ intensity }: { intensity: number }) {
-  const { scene } = useThree();
-  useEffect(() => {
-    scene.environmentIntensity = intensity;
-  }, [scene, intensity]);
-  return <Environment preset="studio" />;
-}
 
 function Loader() {
   return (
@@ -31,15 +22,18 @@ export default function VehicleViewer() {
     <div className="vehicle-viewer">
       <Canvas
         camera={{ position: [2.8, 2.1, 2.8], fov: 50 }}
-        gl={{ toneMappingExposure: 0.3 }}
+        gl={{ toneMappingExposure: 1 }}
+        onCreated={({ scene }) => {
+          scene.environmentIntensity = 0.2;
+        }}
       >
         <color attach="background" args={["#ffffff"]} />
-        <ambientLight intensity={0.05} />
+        <ambientLight intensity={1.0} />
         <directionalLight position={[5, 8, 5]} intensity={0.3} />
         <directionalLight position={[-5, 3, -5]} intensity={0.3} />
         <Suspense fallback={<Loader />}>
           <Tesla />
-          <SceneEnvironment intensity={0.2} />
+          <Environment preset="studio" />
         </Suspense>
         <OrbitControls enablePan={false} enableZoom={false} />
       </Canvas>
